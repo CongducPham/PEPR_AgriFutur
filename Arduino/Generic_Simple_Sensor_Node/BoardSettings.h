3bae2188836f3878cb9bd822f1b3147cea61cbc1
Congduc Pham <Congduc.Pham@univ-pau.fr>
2024-11-17 16:20:35 +0100

    Initial commit

---
+++ Arduino/Generic_Simple_Sensor_Node/BoardSettings.h
@@ -0,0 +1,58 @@
+#ifndef BOARDSETTINGS_H
+  #define BOARDSETTINGS_H
+
+  #if ARDUINO >= 100
+    #include "Arduino.h"
+  #else
+    #include "WProgram.h"
+  #endif
+
+  ////////////////////////////////////////////////////////////////////
+  // uncomment for IRD PCB RAK version where the serial monitor is using software serial with pin 2 for TX
+  // this is the case for the IRD PCBv5
+  //#define SOFT_SERIAL_DEBUG
+  
+  ////////////////////////////////////////////////////////////////////
+  // uncomment for WAZISENSE v2 board
+  // #define WAZISENSE
+
+  ////////////////////////////////////////////////////////////////////
+  // uncomment for IRD PCB board (both v4.1 and v5)
+  #define IRD_PCB
+  // also uncomment for IRD PCB board fully assembled by manufacturer
+  // with all components, including solar circuit (both v4.1 and v5)
+  #define IRD_PCBA
+
+  ////////////////////////////////////////////////////////////////////
+  // uncomment only if the IRD PCB or PCBA is running on solar panel
+  // MUST be commented if running on alkaline battery
+  // code for SOLAR_BAT has been written by Jean-François Printanier from IRD
+  //#define SOLAR_BAT
+  // do not change if you are not knowing what you are doing
+  #define NIMH
+
+  // For powering humdity sensor and DS18B20
+  #if (defined IRD_PCB && defined SOLAR_BAT) || defined IRD_PCBA
+
+    #define PWR_SOFT_START_LOW   20
+    #define PWR_SOFT_START_HIGH  20
+
+    #define PWR_LOW     HIGH  // mosfet P
+    #define PWR_HIGH    LOW   // mosfet P
+
+void power_soft_start( uint8_t pin);
+
+  #else
+
+    #define PWR_LOW     LOW   // gpio
+    #define PWR_HIGH    HIGH  // gpio
+
+  #endif
+
+  #ifdef IRD_PCBA
+    #ifndef IRD_PCB
+      #define IRD_PCB
+    #endif
+  #endif
+
+#endif // BOARDSETTINGS_H
---
+++ Arduino/Generic_Simple_Sensor_Node/SHT_Temperature.cpp
@@ -0,0 +1,90 @@
+/*
+* Copyright (C) 2018-2024 C. Pham, University of Pau, France
+*
+* Congduc.Pham@univ-pau.fr
+*
+*/
+
+#include "BoardSettings.h"
+#include "SHT_Temperature.h"
+
+SHT_Temperature::SHT_Temperature(char* nomenclature, bool is_analog, bool is_connected, bool is_low_power, uint8_t pin_read, uint8_t pin_power, uint8_t pin_trigger):Sensor(nomenclature,is_analog, is_connected, is_low_power, pin_read, pin_power, pin_trigger){
+  if (get_is_connected()){
+  
+    if (get_pin_power()!=-1) {	
+    	pinMode(get_pin_power(),OUTPUT);
+      digitalWrite(get_pin_power(), PWR_LOW);
+
+      if (get_pin_read()!=-1)
+        pinMode(get_pin_read(), INPUT);       
+    
+			if (get_is_low_power())
+       	digitalWrite(get_pin_power(), PWR_LOW);
+    	else
+#if (defined IRD_PCB && defined SOLAR_BAT) || defined IRD_PCBA
+        power_soft_start(get_pin_power());
+#else
+				digitalWrite(get_pin_power(), PWR_HIGH);
+#endif        
+		}
+
+#ifdef SHT3x
+    sht = new Sensirion(get_pin_read(), get_pin_trigger(), 0x45);
+#elif defined SHT2x    
+    sht = new Sensirion(get_pin_read(), get_pin_trigger(), 0x40);
+#else
+    sht = new Sensirion(get_pin_read(), get_pin_trigger());    
+#endif
+      
+    set_warmup_time(500);
+  }
+}
+
+void SHT_Temperature::update_data()
+{
+  if (get_is_connected()) {
+  	
+  	// if we use a digital pin to power the sensor...
+  	if (get_is_low_power() && get_is_power_on_when_active())
+#if (defined IRD_PCB && defined SOLAR_BAT) || defined IRD_PCBA
+      power_soft_start(get_pin_power());
+#else
+    	digitalWrite(get_pin_power(),HIGH);  	
+#endif   	
+
+    // wait
+    delay(get_warmup_time());
+        
+    float t;
+    float h;
+    int8_t ret=0;
+    unsigned long mTime;
+    unsigned long endTime;
+
+    mTime=millis();
+    endTime=mTime+1000;
+
+    while ( (ret != S_Meas_Rdy) && (millis()<endTime) ) {
+      ret=sht->measure(&t, &h);
+    }
+
+    if (ret == S_Meas_Rdy) 
+      set_data((double)t);
+    else 
+      set_data((double)-99.0);
+      
+    if (get_is_low_power() && get_is_power_off_when_inactive())		
+        digitalWrite(get_pin_power(),LOW);   
+  }
+  else {
+  	// if not connected, set a random value (for testing)  	
+  	if (has_fake_data())	
+    	set_data((double)random(-20, 40));
+  }
+
+}
+
+double SHT_Temperature::get_value(){
+  update_data();
+  return get_data();
+}
