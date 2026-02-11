d09fec8c1584000dc3c5950bb7da0b2fd32bba5a
Congduc Pham <Congduc.Pham@univ-pau.fr>
2026-02-11 13:17:45 +0100

    Update tutorials & fix bug in delete_device.sh

--- Gateway/scripts/delete_device.sh
+++ Gateway/scripts/delete_device.sh
@@ -11,11 +11,11 @@ fi
 echo "Get token"
 TOK=`curl -X POST "http://localhost/auth/token" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"username\":\"admin\",\"password\":\"loragateway\"}" | tr -d '\"'`
 
-echo "Delete device ${DEVICE}"
+echo "Delete device ${1}"
 #delete on IIWA
-curl -X DELETE "http://localhost:5000/devices/${DEVICE}" -H  "accept: application/json"
+curl -X DELETE "http://localhost:5000/devices/${1}" -H  "accept: application/json"
 #delete on WaziGate
-curl -X DELETE "http://localhost/devices/${DEVICE}" -H "accept: application/json" -H "Authorization: Bearer $TOK" -H  "Content-Type: application/json"   
+curl -X DELETE "http://localhost/devices/${1}" -H "accept: application/json" -H "Authorization: Bearer $TOK" -H  "Content-Type: application/json"   
 
 echo "Checking delete operation"
 curl -X GET "http://localhost/devices" -H  "accept: application/json"
